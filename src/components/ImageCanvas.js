557042315fd973054514fd0b3c276b4fe9b25157
Vincenzo Cassaro <vincenzocassaro1@gmail.com>
2023-02-25 15:37:49 +0100

    drawing panels work!

--- src/components/ImageCanvas.js
+++ src/components/ImageCanvas.js
@@ -3,17 +3,22 @@ import { useEffect, useRef, useState } from 'react';
 const ImageCanvas = ({ data }) => {
     const canvasRef = useRef(null);
     const [images, setImages] = useState([]);
-    const [currentIndex, setCurrentIndex] = useState(0);
+    const [currentPageIndex, setCurrentPageIndex] = useState(0);
+    const [currentPanelIndex, setCurrentPanelIndex] = useState(0);
+    const [panelsInThisPage, setpanelsInThisPage] = useState(0);
 
+    // load data
     useEffect(() => {
         const loadedImages = [];
         let loadedCount = 0;
+
         data.pages.forEach((element, index) => {
             const image = new Image();
             image.onload = () => {
                 loadedCount++;
                 if (loadedCount === data.pages.length) {
                     setImages(loadedImages);
+                    // setData(data);
                 }
             };
             image.src = element.image;
@@ -21,30 +26,86 @@ const ImageCanvas = ({ data }) => {
         });
     }, []);
 
+    function handleRightArrow() {
+        setCurrentPanelIndex((prevIndex) => {
+            if (prevIndex === panelsInThisPage - 1) {
+                console.log('Go to next page');
+                setCurrentPageIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+                return 0;
+            } else {
+                return prevIndex + 1
+            }
+        });
+        // console.log(currentPanelIndex)
+        // setCurrentPanelIndex((prevIndex) => (prevIndex === panelsInThisPage - 1 ? 0 : prevIndex + 1));
+    }
+
+    // handle arrow keys
     useEffect(() => {
         const handleKeyDown = (event) => {
             if (event.key === 'ArrowLeft') {
                 // todo: fix loop
-                setCurrentIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+                setCurrentPageIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
             } else if (event.key === 'ArrowRight') {
-                setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+                handleRightArrow();
             }
         };
         window.addEventListener('keydown', handleKeyDown);
         return () => {
             window.removeEventListener('keydown', handleKeyDown);
         };
-    }, [images]);
+    }, [currentPanelIndex, images]);
 
+    // draw things
     useEffect(() => {
+        let panels_in_this_page = data.pages[currentPageIndex].panels.length;
+        console.log("____panels_in_this_page", panels_in_this_page);
+        setpanelsInThisPage(panels_in_this_page)
+
         const ctx = canvasRef.current.getContext('2d');
         ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
         if (images.length > 0) {
-            const currentImage = images[currentIndex];
+            const currentImage = images[currentPageIndex];
 
-            ctx.drawImage(currentImage, 0, 0, currentImage.width / currentImage.height * window.innerHeight, window.innerHeight);
+            const max = data.pages[currentPageIndex].panels.length;
+            const panels_len = Math.min(Math.max(parseInt(currentPanelIndex + 1), 0), max);
+
+            // Because of the way canvas works we need all panels up
+            // to and including the desired one.
+            for (var i = 0; i < panels_len; i++) {
+                // The path needs to be split in order to work with it.
+                const path = data.pages[currentPageIndex].panels[i].path.split(',');
+                const len = path.length;
+
+                ctx.save();
+
+                // First we draw a clipping path for the panel
+                ctx.beginPath();
+                for (var j = 0; j < len; j++) {
+                    const coards = path[j].trim().split(' ');
+
+                    // The svg path's coardinates commands need to be in pixels
+                    // instead of percentages. Svg path do not work with %s.
+                    const x = coards[0] * currentImage.width / currentImage.height * window.innerHeight / 100;
+                    const y = coards[1] * window.innerHeight / 100;
+
+                    // The first element in the path needs to
+                    // be the M(ove) command/
+                    if (len == 0) {
+                        ctx.moveTo(x, y);
+                    } else {
+                        ctx.lineTo(x, y);
+                    }
+                }
+                ctx.closePath();
+                ctx.clip();
+
+                ctx.drawImage(currentImage, 0, 0, currentImage.width / currentImage.height * window.innerHeight, window.innerHeight);
+
+                ctx.restore();
+            }
         }
-    }, [currentIndex, images]);
+    }, [currentPageIndex, currentPanelIndex, images]);
 
     return (
         <canvas id="image-canvas" ref={canvasRef} width={window.innerWidth} height={window.innerHeight}></canvas>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import ImageCanvas from "../components/ImageCanvas";
 
 
