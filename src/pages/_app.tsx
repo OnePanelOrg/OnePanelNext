7b3784917db3f699b57994c6b36f0bafda0c307b
Vincenzo Cassaro <vincenzocassaro1@gmail.com>
2023-12-28 18:52:11 +0100

    added analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,25 @@
 import {
-  ClerkProvider, SignedIn,
+  ClerkProvider,
+  SignedIn,
   SignedOut,
   SignInButton,
-  UserButton
-} from '@clerk/nextjs';
+  UserButton,
+} from "@clerk/nextjs";
 import { type AppType } from "next/dist/shared/lib/utils";
 import Link from "next/link";
 import "../styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 function Header() {
   return (
     <header
       style={{ display: "flex", justifyContent: "space-between", padding: 20 }}
     >
-      <h1><Link
-        className=""
-        href={"/"}
-      >
-        OnePanel
-      </Link></h1>
+      <h1>
+        <Link className="" href={"/"}>
+          OnePanel
+        </Link>
+      </h1>
       <SignedIn>
         {/* Mount the UserButton component */}
         <UserButton />
@@ -31,13 +32,12 @@ function Header() {
   );
 }
 
-
-
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <ClerkProvider {...pageProps} >
+    <ClerkProvider {...pageProps}>
       {/* <Header /> */}
       <Component {...pageProps} />
+      <Analytics />
     </ClerkProvider>
   );
 };
