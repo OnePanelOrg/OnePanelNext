46ddf200e832819f3f79ee54e7fb3f80717512fb
Vincenzo Cassaro <vincenzocassaro1@gmail.com>
2024-04-02 22:19:35 +0200

    changed tcb hostname

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ const Home: NextPage = () => {
     if (!chapter_url) {
       return true;
     }
-    if (!chapter_url.includes("https://tcbscans.com/")) {
+    if (!chapter_url.includes("https://tcb-backup.bihar-mirchi.com/")) {
       return true;
     }
     return false;
