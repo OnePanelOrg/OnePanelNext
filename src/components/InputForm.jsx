a68bf602787bcb59c00c71ead3e778cb20b19edb
Vincenzo Cassaro <vincenzocassaro1@gmail.com>
2024-10-18 17:39:30 +0200

    fix placeholder

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -35,7 +35,7 @@ export default function InputForm({ childToParent }) {
                       id="username"
                       autoComplete="username"
                       className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                      placeholder="https://tcb-backup.bihar-mirchi.com/something"
+                      placeholder="chapter url
                       onChange={(event) => setChapterValue(event.target.value)}
                     />
                   </div>
